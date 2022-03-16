20b7c755e4ebb2a30edd92dc3ec10697b4953fd6
Stefanus Albert Kosim <albertkosim24@gmail.com>
2022-03-16 10:45:54 +0700

    update keep-alive

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,18 @@
 import helper.FileHelper
 import helper.HtmlHelper
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.io.*
-import java.net.InetAddress
-import java.net.ServerSocket
+import java.net.*
+import java.util.*
 
 var staticRootDir: String = System.getProperty("user.dir")
 var rootDir: String = staticRootDir
 var host: String? = ""
-val ttl = 100
-val timeout = 15
+const val max = 100
+const val timeout = 17
 var keepAlive = false
-
+var isSent = false
 
 fun main() = runBlocking {
     val inputStream: InputStream = File("$rootDir\\progjarx.conf").inputStream()
@@ -24,20 +25,22 @@ fun main() = runBlocking {
 
     val serverIP = config[0].substringBefore(':')
     val serverPort = config[0].substringAfter(':').toInt()
-    val server = ServerSocket(serverPort, 5, InetAddress.getByName(serverIP))
+    val server = ServerSocket()
+    val endPoint = InetSocketAddress(InetAddress.getByName(serverIP), serverPort)
+    server.bind(endPoint)
     println("Server is active, listening at port: $serverPort")
     while (true) {
         println("= = = = = Waiting for next client to connect = = = = =")
         val client = server.accept()
-        println("= = = = = connected to ${client.inetAddress} = = = = =")
+//        println("= = = = = connected to $host = = = = =")
 
         keepAlive = false
 
-        val br = BufferedReader(InputStreamReader(client.getInputStream()))
-        val bw = BufferedWriter(OutputStreamWriter(client.getOutputStream()))
-        val bos = BufferedOutputStream(client.getOutputStream())
+        var br = BufferedReader(InputStreamReader(client.getInputStream()))
+        var bw = BufferedWriter(OutputStreamWriter(client.getOutputStream()))
+        var bos = BufferedOutputStream(client.getOutputStream())
 
-        val header = getRequestHeader(br)
+        var header = getRequestHeader(br, client)
 
         rootDir = staticRootDir
 
@@ -51,123 +54,79 @@ fun main() = runBlocking {
             }
         }
 
-        val firstRowResponses = header.substringBefore("\n", "").split(" ")
-        println(firstRowResponses)
+        var firstRowResponses = header.substringBefore("\n", "").split(" ")
+//        println(firstRowResponses)
         if (firstRowResponses.size < 3) {
             client.close()
             continue
         }
 
-        val url = firstRowResponses[1].removePrefix("/")
-        val file = File(rootDir, "\\$url")
-        println(url)
+        var url = firstRowResponses[1].removePrefix("/")
+        var file = File(rootDir, "\\$url")
+        println("url: $url")
 
 //        println("= = = = = request from client = = = = =")
 //        print(header)
 //        println("= = = = = end of request = = = = =")
+        try {
+            if(keepAlive) {
+                response(file, bw, bos, url)
+                println("KeepAlive")
 
-        val responseHeader: String
-        val mimeType: String
-        when {
-            file.exists() && file.isFile -> {
-                println("accessing ${file.path}")
-                mimeType = FileHelper.getMimeType(file)
-//                println("absolutePath ${file.absolutePath}")
-//                println("mimeType: $mimeType")
-                responseHeader = responseHeaderBuilder(
-                    code = 200,
-                    status = "OK",
-                    contentType = mimeType,
-                    contentLength = file.length(),
-                )
-
-                with(bw) {
-                    write(responseHeader)
-                    flush()
-                }
-                file.inputStream().use {
-                    val byteArray = ByteArray(1024 * 8)
-                    var read = it.read(byteArray)
-                    while (true) {
-                        bos.write(byteArray, 0, read)
-                        read = it.read(byteArray)
-                        if (read < 0) {
-                            break
+                    while(true) {
+                        if(isSent) {
+                            keepAlive = false
+//                            println("\nSending Responsss\n")
+                            br = BufferedReader(InputStreamReader(client.getInputStream()))
+                            bw = BufferedWriter(OutputStreamWriter(client.getOutputStream()))
+                            bos = BufferedOutputStream(client.getOutputStream())
+                            header = getRequestHeader(br)
+                            rootDir = staticRootDir
+                            var path: String
+                            for (i in 1 until config.size) {
+                                if(config[i].substringBefore(':') == host) {
+                                    path = config[i].substringAfter(":./")
+                                    rootDir += "\\$path"
+                                    break
+                                }
+                            }
+                            if(header == "null") {
+    //                            println("x")
+                                break
+                            }
+                            firstRowResponses = header.substringBefore("\n", "").split(" ")
+    //                        println("Frist RWO RSPONS : " + firstRowResponses)
+                            if(firstRowResponses.size < 3) {
+                                break
+                            }
+                            else {
+                                url = firstRowResponses[1].removePrefix("/")
+                            }
+                            file = File(rootDir, "\\$url")
+                            println("url: $url")
+                            isSent = false
+                            response(file, bw, bos, url)
+//                            println("end of responsss")
                         }
                     }
-                    bos.flush()
-                }
-            }
-            file.exists() && file.isDirectory -> {
-                println("accessing ${file.path}")
-                val fileDataList = FileHelper.getAllFileAndDir(file)
-                val content: String
-
-                mimeType = "text/html; charset=UTF-8"
-                val indexFileData = fileDataList.find {
-                    it.name == "index.html" ||
-                    it.name == "index.php"
-                }
-
-                responseHeader = if (indexFileData != null && !indexFileData.isDirectory) {
-                    val indexFile = File(indexFileData.absolutePath)
-                    var temp = ""
-                    indexFile.readLines().forEach {
-                        temp += "$it\n"
-                    }
-                    content = temp
-                    var location = "http://$host"
-                    if (url.isNotBlank()) {
-                        location += "/$url"
-                    }
-                    location += "/${indexFileData.name}"
-                    responseHeaderBuilder(
-                        code = 302,
-                        status = "OK",
-                        contentType = mimeType,
-                        contentLength = content.length.toLong(),
-                        location = location
-                    )
-                } else {
-                    content = HtmlHelper.generateListingHtml(file, fileDataList)
-                    responseHeaderBuilder(
-                        code = 200,
-                        status = "OK",
-                        contentType = mimeType,
-                        contentLength = content.length.toLong(),
-                    )
-                }
-
-                with(bw) {
-                    write(responseHeader)
-                    flush()
-                }
-                with(bos) {
-                    write(content.toByteArray(), 0, content.length)
-                    flush()
-                }
-            }
-            else -> {
-                val content = "<!doctype html><html><h1>hello mom!</h1><h3>404 Not found, too bad.</h3></html>\r\n"
-                mimeType = "text/html; charset=UTF-8"
-                responseHeader = responseHeaderBuilder(
-                    code = 404,
-                    status = "Not found",
-                    contentType = mimeType,
-                    contentLength = content.length.toLong(),
-                )
-                with(bw) {
-                    write(responseHeader)
-                    flush()
-                }
-                with(bos) {
-                    write(content.toByteArray(), 0, content.length)
-                    flush()
-                }
+            } else {
+                response(file, bw, bos, url)
             }
         }
-//        if(keepAlive) delay(3000)
-        client.close()
+        catch (e: SocketTimeoutException) {}
+        catch (e: Exception) {
+            System.err.println("Server error: " + e.message)
+        } finally {
+            // Close the connection
+            try {
+                client.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            System.out.format("[%s] Closing client access\n", Date())
+        }
+
+
 
 //        println("= = = = = header from server = = = = =")
 //        print(responseHeader)
@@ -179,7 +138,114 @@ fun main() = runBlocking {
 //    print("Server crashed with ${e.message}")
 //}
 
-fun getRequestHeader(br: BufferedReader): String {
+fun response(file: File, bw: BufferedWriter, bos: BufferedOutputStream, url: String) {
+    val responseHeader: String
+    val mimeType: String
+    when {
+        file.exists() && file.isFile -> {
+            println("accessing ${file.path}")
+            mimeType = FileHelper.getMimeType(file)
+//                println("absolutePath ${file.absolutePath}")
+//                println("mimeType: $mimeType")
+            responseHeader = responseHeaderBuilder(
+                code = 200,
+                status = "OK",
+                contentType = mimeType,
+                contentLength = file.length(),
+            )
+
+            with(bw) {
+                write(responseHeader)
+                flush()
+            }
+            file.inputStream().use {
+                val byteArray = ByteArray(1024 * 8)
+                var read = it.read(byteArray)
+                while (true) {
+                    bos.write(byteArray, 0, read)
+                    read = it.read(byteArray)
+                    if (read < 0) {
+                        break
+                    }
+                }
+                bos.flush()
+            }
+        }
+        file.exists() && file.isDirectory -> {
+            println("accessing ${file.path}")
+            val fileDataList = FileHelper.getAllFileAndDir(file)
+            val content: String
+
+            mimeType = "text/html; charset=UTF-8"
+            val indexFileData = fileDataList.find {
+                it.name == "index.html"
+            }
+
+            responseHeader = if (indexFileData != null && !indexFileData.isDirectory) {
+                val indexFile = File(indexFileData.absolutePath)
+                var temp = ""
+                indexFile.readLines().forEach {
+                    temp += "$it\n"
+                }
+                content = temp
+                var location = "http://$host"
+                if (url.isNotBlank()) {
+                    location += "/$url"
+                }
+                location += "/${indexFileData.name}"
+                responseHeaderBuilder(
+                    code = 302,
+                    status = "OK",
+                    contentType = mimeType,
+                    contentLength = content.length.toLong(),
+                    location = location
+                )
+            } else {
+                content = HtmlHelper.generateListingHtml(file, fileDataList)
+                responseHeaderBuilder(
+                    code = 200,
+                    status = "OK",
+                    contentType = mimeType,
+                    contentLength = content.length.toLong(),
+                )
+            }
+
+            with(bw) {
+                write(responseHeader)
+                flush()
+            }
+            with(bos) {
+                write(content.toByteArray(), 0, content.length)
+
+                    flush()
+            }
+        }
+        else -> {
+            val content = "<!doctype html><html><h1>hello mom!</h1><h3>404 Not found, too bad.</h3></html>\r\n"
+            mimeType = "text/html; charset=UTF-8"
+            responseHeader = responseHeaderBuilder(
+                code = 404,
+                status = "Not found",
+                contentType = mimeType,
+                contentLength = content.length.toLong(),
+            )
+            with(bw) {
+                write(responseHeader)
+                flush()
+            }
+            with(bos) {
+                write(content.toByteArray(), 0, content.length)
+                flush()
+            }
+        }
+    }
+//    println("respons header")
+//    println(responseHeader)
+//    println("end respons header")
+    isSent = true
+}
+
+fun getRequestHeader(br: BufferedReader, client: Socket): String {
     var header = ""
 
     while (true) {
@@ -190,6 +256,10 @@ fun getRequestHeader(br: BufferedReader): String {
         }
         if(message != null && message.contains("keep-alive")) {
             keepAlive = true
+            client.keepAlive = true
+            client.tcpNoDelay = true
+            client.soTimeout = (timeout * 1000)
+            break
         }
         if (message == null || message == "\r\n" || message.isBlank()) {
             break
@@ -197,6 +267,31 @@ fun getRequestHeader(br: BufferedReader): String {
         header += "$message\n"
     }
 
+//    println("=================HEADER=================")
+//    println(header)
+//    println("=================END OF HEADER=================")
+    return header
+}
+
+fun getRequestHeader(br: BufferedReader): String {
+    var header = ""
+
+    while (true) {
+        val message: String? = br.readLine()
+
+//        println(message)
+        if(message != null && message.contains("Host")) {
+            host = message.substringAfter(' ')
+            break
+        }
+        if (message == null || message == "\r\n" || message.isBlank()) {
+            break
+        }
+        header += "$message\n"
+    }
+//    println("=================HEADER=================")
+//    println(header)
+//    println("=================END OF HEADER=================")
     return header
 }
 
@@ -241,7 +336,7 @@ fun responseHeaderBuilder(
     response += "Content-Type: $contentType\r\n"
     response += "Content-Length: $contentLength\r\n"
     if(keepAlive) {
-        response += "Keep-Alive: timeout=$timeout, max=$ttl\r\n"
+        response += "Keep-Alive: timeout=$timeout, max=$max\r\n"
         response +="Connection: Keep-Alive\r\n"
     }
     response += "Server: progjarx/v2.0\r\n"
